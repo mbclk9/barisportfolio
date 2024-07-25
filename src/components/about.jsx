55a082077e5c99c8485b17afd2fef351e568301c
mbclk9 <mbclk94@gmail.com>
2024-07-25 11:47:38 +0300

    about kısmı çözüldü

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,11 +4,11 @@ import {motion} from "framer-motion"
 
 export default function About(){
     return(
-        <div className="border-b border-neutral-900 pb-4 container">
+        <div className="border-b border-neutral-900 pb-4 ">
           <h2 className="my-20 text-center text-4xl">About 
             <span className=" text-neutral-500"> ME</span>
           </h2>
-          <div className="flex flex-wrap">
+          <div className="flex flex-wrap ">
             <div className="w-full lg:w-1/2 lg:p-8">
               <div className="flex items-center justify-center">
                 <motion.img 
@@ -20,8 +20,8 @@ export default function About(){
             <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:justify-start">
                     <motion.p 
-                    whileInView={{opacity:1, x:-100}}
-                    initial={{x:0, opacity:0}}  transition={{duration:0.5}}
+                    whileInView={{opacity:1, x:0}}
+                    initial={{x:100, opacity:0}}  transition={{duration:0.5}}
                     className="my-2 max-w-xl py-6 font-normal tracking-normal items-center m-auto  "> {About_Content} </motion.p>
                 </div>
               </div>
